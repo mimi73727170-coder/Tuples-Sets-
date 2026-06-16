c4d3cf3f8acfe1c7424fdfc625664c81f1c4c989
Mimi <mimi73727170@gmail.com>
2026-06-17 05:21:31 +0530

    Add files via upload

---
+++ Tuples & Sets.py
@@ -0,0 +1,16 @@
+fruits = ["Apple","Banana","Pineapple"]
+fruits.append("Mango")
+print(fruits)
+
+
+numbers = {1,2,3,4,5,6,7,8,9,3,6,1,7,5}
+print(numbers)
+
+subjects = ("Physics","Chemistry","Math")
+print(subjects[0])
+
+marks = {23,74,97,75,23,73,16,19,33,48}
+print(marks)
+
+skills = {"Python","Fashion Desigh","Presentation"}
+print(skills)
